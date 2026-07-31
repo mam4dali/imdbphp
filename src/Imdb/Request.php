f00c03662075b30198d3ef3b6aa5d3e6fff2d0fd
MOHAMMAD Ali Heidari <h.mohammad026@gmail.com>
2026-07-31 14:09:54 +0330

    Revert "Add AWS WAF challenge solver"
    This reverts commit 040428f187b4de82f5be1a231886fa91996a17fd.

--- src/Imdb/Request.php
+++ src/Imdb/Request.php
@@ -25,10 +25,6 @@ class Request
     private $requestHeaders = array();
     private $responseHeaders = array();
     private $config;
-    private $userAgent;
-    private $postContent = null;
-    private $maxChallengeRetries = 3;
-    private static $wafToken = null;
 
     /**
      * No need to call this.
@@ -42,12 +38,6 @@ class Request
         curl_setopt($this->ch, CURLOPT_ENCODING, "");
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->ch, CURLOPT_HEADERFUNCTION, array(&$this, "callback_CURLOPT_HEADERFUNCTION"));
-        if (self::$wafToken === null) {
-            self::$wafToken = self::loadWafToken($config);
-        }
-        if (self::$wafToken !== null) {
-            curl_setopt($this->ch, CURLOPT_COOKIE, "aws-waf-token=" . self::$wafToken);
-        }
 
         //use HTTP-Proxy
         if ($config->use_proxy === true) {
@@ -65,8 +55,11 @@ class Request
 
         $this->addHeaderLine('Referer', 'https://' . $config->imdbsite . '/');
 
-        $this->userAgent = $config->force_agent ?: $config->default_agent;
-        curl_setopt($this->ch, CURLOPT_USERAGENT, $this->userAgent);
+        if ($config->force_agent) {
+            curl_setopt($this->ch, CURLOPT_USERAGENT, $config->force_agent);
+        } else {
+            curl_setopt($this->ch, CURLOPT_USERAGENT, $config->default_agent);
+        }
         if ($config->language) {
             $this->addHeaderLine('Accept-Language', $config->language);
         }
@@ -89,7 +82,6 @@ class Request
      */
     public function post($content)
     {
-        $this->postContent = $content;
         curl_setopt($this->ch, CURLOPT_POST, true);
         curl_setopt($this->ch, CURLOPT_POSTFIELDS, $content);
         return $this->sendRequest();
@@ -105,106 +97,14 @@ class Request
         $this->responseHeaders = array();
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->requestHeaders);
         $this->page = curl_exec($this->ch);
-        $httpCode = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
         curl_close($this->ch);
-
-        if ($this->page === false) {
-            if ($this->config->throwHttpExceptions) {
-                throw new Exception\Http("Failed fetch url [$this->urltoopen] " . curl_error($this->ch));
-            }
-            return false;
+        if ($this->page !== false) {
+            return true;
         }
-
-        if ($httpCode == 202) {
-            return $this->solveAwsChallenge();
+        if ($this->config->throwHttpExceptions) {
+            throw new Exception\Http("Failed fetch url [$this->urltoopen] " . curl_error($this->ch));
         }
-
-        return true;
-    }
-
-    private function fetchChallengeHtml()
-    {
-        $ch = curl_init('https://' . $this->config->imdbsite . '/');
-        curl_setopt($ch, CURLOPT_ENCODING, "");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-            'accept-language: en-US,en;q=0.9',
-            'cache-control: no-cache',
-            'pragma: no-cache',
-            'sec-ch-ua: "Chromium";v="144", "Google Chrome";v="144"',
-            'sec-ch-ua-mobile: ?0',
-            'sec-ch-ua-platform: "macOS"',
-            'sec-fetch-dest: document',
-            'sec-fetch-mode: navigate',
-            'sec-fetch-site: none',
-            'upgrade-insecure-requests: 1',
-        ));
-        if ($this->config->use_proxy === true) {
-            curl_setopt($ch, CURLOPT_PROXY, $this->config->proxy_host);
-            curl_setopt($ch, CURLOPT_PROXYPORT, $this->config->proxy_port);
-            if (!empty($this->config->proxy_user) && !empty($this->config->proxy_pw)) {
-                curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-                curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->config->proxy_user . ':' . $this->config->proxy_pw);
-            }
-        }
-        $html = curl_exec($ch);
-        curl_close($ch);
-        return $html;
-    }
-
-    private function solveAwsChallenge()
-    {
-        for ($attempt = 0; $attempt < $this->maxChallengeRetries; $attempt++) {
-            $challengeHtml = $this->page;
-            if (empty($challengeHtml) || strpos($challengeHtml, 'window.gokuProps') === false) {
-                $challengeHtml = $this->fetchChallengeHtml();
-            }
-            if (empty($challengeHtml) || strpos($challengeHtml, 'window.gokuProps') === false) {
-                continue;
-            }
-            $aws = new Aws($this->userAgent, $this->config->imdbsite, $this->config);
-            $token = $aws->solve($challengeHtml);
-            if (empty($token)) {
-                continue;
-            }
-
-            $this->ch = curl_init($this->urltoopen);
-            curl_setopt($this->ch, CURLOPT_ENCODING, "");
-            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($this->ch, CURLOPT_HEADERFUNCTION, array(&$this, "callback_CURLOPT_HEADERFUNCTION"));
-            curl_setopt($this->ch, CURLOPT_USERAGENT, $this->userAgent);
-            curl_setopt($this->ch, CURLOPT_COOKIE, "aws-waf-token=" . $token);
-            curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->requestHeaders);
-            if ($this->postContent !== null) {
-                curl_setopt($this->ch, CURLOPT_POST, true);
-                curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->postContent);
-            }
-
-            if ($this->config->use_proxy === true) {
-                curl_setopt($this->ch, CURLOPT_PROXY, $this->config->proxy_host);
-                curl_setopt($this->ch, CURLOPT_PROXYPORT, $this->config->proxy_port);
-                if (!empty($this->config->proxy_user) && !empty($this->config->proxy_pw)) {
-                    curl_setopt($this->ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-                    curl_setopt($this->ch, CURLOPT_PROXYUSERPWD, $this->config->proxy_user . ':' . $this->config->proxy_pw);
-                }
-            }
-
-            $this->responseHeaders = array();
-            $this->page = curl_exec($this->ch);
-            $retryCode = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
-            curl_close($this->ch);
-
-            if ($this->page !== false && $retryCode != 202) {
-                self::$wafToken = $token;
-                self::saveWafToken($token, $this->config);
-                return true;
-            }
-        }
-
-        return true;
+        return false;
     }
 
     /**
@@ -299,27 +199,5 @@ class Request
             $this->responseHeaders[] = $str;
         }
         return $len;
-    }
-
-    private static function wafTokenPath(Config $config)
-    {
-        $dir = !empty($config->cachedir) && is_dir($config->cachedir)
-            ? $config->cachedir
-            : sys_get_temp_dir();
-        return rtrim($dir, '/') . '/imdb_waf_token';
-    }
-
-    private static function loadWafToken(Config $config)
-    {
-        $path = self::wafTokenPath($config);
-        if (file_exists($path) && (time() - filemtime($path)) < 280) {
-            return file_get_contents($path);
-        }
-        return null;
-    }
-
-    private static function saveWafToken($token, Config $config)
-    {
-        @file_put_contents(self::wafTokenPath($config), $token);
     }
 }
